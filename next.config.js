ee01246badb969d8d1bb2c74ffe8979c679d5384
shota max nitta <dustbox.1218@gmail.com>
2023-03-24 12:06:27 +0900

    update build settings

--- next.config.js
+++ next.config.js
@@ -3,4 +3,15 @@ const withNextra = require('nextra')({
   themeConfig: './theme.config.tsx',
 })
 
+<<<<<<< Updated upstream
 module.exports = withNextra()
+=======
+const isProd = process.env.NODE_ENV === 'production';
+
+module.exports = withNextra({
+  images: {unoptimized: true},
+  basePath: isProd ? '/your-repo-name' : '',
+  assetPrefix: isProd ? '/your-repo-name/' : '',
+  trailingSlash: true
+})
+>>>>>>> Stashed changes
